96a807ab42039c4eb7e5e1e652ddc9c0381c2796
Hery <Hery>
2023-04-30 00:51:54 +0700

    feat(register): Added register function
    Added register function with post method and response with the data given.
    
    Feat #2

--- go-register/main.go
+++ go-register/main.go
@@ -1,9 +1,51 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// err := db.Connect()
+
+	// if err != nil {
+	// 	log.Println("Failed to connect to DB")
+	// }
+
+	// server.Init()
+
 	router := gin.Default()
 
+	router.POST("/register", RegisterHandler)
+
 	router.Run()
+}
+
+type RegisterReq struct {
+	Phone string
+	Name  string
+	Role  string
+}
+
+// type RegisterRes struct {
+// 	Phone string
+// 	Name string
+// 	Role string
+// 	Password int
+// }
+
+func RegisterHandler(c *gin.Context) {
+	var registerReq RegisterReq
+
+	err := c.ShouldBindJSON(&registerReq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"Phone": registerReq.Phone,
+		"Name":  registerReq.Name,
+	})
 }
