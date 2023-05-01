226abafad2bfaed2b17bd7f07ebd2b524dc6b41d
Hery <22.hery86@gmail.com>
2023-05-01 07:25:31 +0700

    feat(FetchApp): Iniatialize Fetch.
    Feat #8

--- FetchApp/index.js
+++ FetchApp/index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const mysql = require('mysql2')
 const app = express()
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'super-password',
+    database: 'testExpress',
+});
 
 app.get('/fetch', (req, res) => {
     res.send('fetched')
