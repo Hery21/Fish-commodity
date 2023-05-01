232460b008116a867529c73f4b4cfd59a6dee547
Hery <22.hery86@gmail.com>
2023-05-01 07:54:05 +0700

    feat(function): Added convert to USD function
    Added a function to convert IDR to USD using API and caching.
    
    Feature #12

--- FetchApp/index.js
+++ FetchApp/index.js
@@ -25,6 +25,44 @@ function authorizeJWT(req, res, next) {
     });
 }
 
+function convertToUSD(priceInIDR, callback) {
+    const cacheKey = `IDR_USD_${priceInIDR}`;
+    const cachedValue = cache.get(cacheKey);
+  
+    if (cachedValue) {
+        callback(cachedValue);
+    } else {
+        const url = `https://v6.exchangerate-api.com/v6/ed2ec4e8a73820df3b7dc7b5/latest/USD`;
+    
+        https.get(url, function(response) {
+            let data = '';
+    
+            response.on('data', function(chunk) {
+            data += chunk;
+            });
+    
+            response.on('end', function() {
+            try {
+                const jsonData = JSON.parse(data);
+                const conversionRate = jsonData["conversion_rates"]["IDR"];
+                const priceInUSD = priceInIDR / conversionRate;
+    
+                const roundedValue = priceInUSD.toFixed(2);
+                cache.set(cacheKey, roundedValue);
+    
+                callback(roundedValue);
+            } catch (error) {
+                console.error(error);
+                callback(null);
+            }
+            });
+        }).on('error', function(error) {
+            console.error(error);
+            callback(null);
+        });
+    }
+}
+
 app.get('/fetch', authorizeJWT, (req, res) => {
     https.get('https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/list', (response) => {
         let data = ''
