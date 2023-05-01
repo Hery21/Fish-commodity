8b6d56356cb19406130637a7fcf06a26b80e4212
Hery <22.hery86@gmail.com>
2023-05-01 07:29:59 +0700

    feat(endpoint): Added fetch endpoint
    Added fetch endpoint and insert into table in the fish_commodity database.
    
    Feat #9

--- FetchApp/index.js
+++ FetchApp/index.js
@@ -10,5 +10,49 @@ const connection = mysql.createConnection({
 });
 
 app.get('/fetch', (req, res) => {
-    res.send('fetched')
+    https.get('https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/list', (response) => {
+        let data = ''
+
+        response.on('data', (chunk)=> {
+            data += chunk
+        })
+
+        response.on('end', function() {
+            const jsonData = JSON.parse(data);
+        
+            let index = 0;
+        
+            function insertNextRow() {
+              if (index < jsonData.length) {
+                convertToUSD(jsonData[index].price, function(priceUsd) {
+                  connection.execute(
+                    'INSERT INTO prices (uuid, komoditas, area_provinsi, size, price, price_usd, tgl_parsed) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                    [jsonData[index].uuid, jsonData[index].komoditas, jsonData[index].area_provinsi, jsonData[index].size, jsonData[index].price, priceUsd, new Date(jsonData[index].tgl_parsed)],
+                    function(error) {
+                      if (error) {
+                        console.error(error);
+                      } else {
+                        index++;
+                        insertNextRow();
+                        if (index === jsonData.length) {
+                            console.log('Data inserted successfully');
+                            connection.close();
+                            res.status(200).send('Data inserted successfully');
+                        }
+                      }
+                    }
+                  );
+                });
+              } else {
+                console.log('Data inserted successfully');
+                connection.close();
+              }
+            }
+        
+            insertNextRow();
+          });
+    }).on('error', (error) => {
+        console.error(error)
+        res.status(500).send('Error fetching data')
+    })
 })
